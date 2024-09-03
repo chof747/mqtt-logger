50d7db6a7b70c65cba31c0ea61074322ae57f958
Christian Hofbauer <chof@gmx.at>
2024-09-03 22:08:26 +0200

    updated accepting also python 3.11

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.12',
+    python_requires='>=3.11',
     install_requires=[
       'paho-mqtt==2.1.0'
     ],
